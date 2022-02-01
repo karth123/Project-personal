ca42dcc08db1a975e595ad89e91bf7fef5cfaac2
Karthik Prabhu <khanadvocate456@gmail.com>
2022-02-01 11:30:18 +0530

    Create Expected_val_prob.py
    Solved a problem

---
+++ Python/Expected_val_prob.py
@@ -0,0 +1,19 @@
+import random
+N = int(input("Enter the number of trials (enter a large number to get an accurate expected value)"))
+def rand():
+    i = 0
+    k = 0
+    while i<1:
+        k=k+1
+        i = i +random.random()
+    return k
+x = sum(rand() for _ in range(N))/N
+print(x)
+
+
+
+
+
+
+
+
