668f31ad66cf027b3887fe6e9033a57747ce87da
Karthik Prabhu <khanadvocate456@gmail.com>
2022-02-01 11:34:16 +0530

    Update Expected_val_P1.py

--- Python/Expected_val_P1.py
+++ Python/Expected_val_P1.py
@@ -1,5 +1,5 @@
 import random
-N = int(input("Enter the number of trials (enter a large number to get an accurate expected value)"))
+N = int(input("Enter the number of trials (enter a large number to get an accurate expected value) "))
 def rand():
     i = 0
     k = 0
@@ -7,8 +7,7 @@ def rand():
         k=k+1
         i = i +random.random()
     return k
-x = sum(rand() for _ in range(N))/N
-print(x)
+print(sum(rand() for _ in range(N))/N)
 
 
 
